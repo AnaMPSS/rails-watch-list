5d674c47dff1ea2c3de045885acf90ddbed33d26
Ana Sousa <anapaisesousa@gmail.com>
2022-11-17 12:50:01 -0100

    set up done

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,10 +6,7 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
-gem "autoprefixer-rails"
-gem "font-awesome-sass", "~> 6.1"
-gem "simple_form", github: "heartcombo/simple_form"
-gem "sassc-rails"
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
